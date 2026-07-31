74666a7666304d282692459403c8e1c29ef0c841
abkhoshbakht-hub <ab.khoshbakht@gmail.com>
2026-07-31 15:07:09 +0330

    Initial Genesis Framework

---
+++ genesis/main.py
@@ -0,0 +1,10 @@
+def main():
+    print("=" * 50)
+    print("Genesis Framework")
+    print("Version : 1.0.0")
+    print("Status  : Running")
+    print("=" * 50)
+
+
+if __name__ == "__main__":
+    main()
